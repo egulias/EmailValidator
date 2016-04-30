f1116d322d41ed0f32f814ed11baf2bd3d51839e
Eduardo Gulias Davis <me@egulias.com>
2016-04-30 16:29:44 +0200

    Spoof validation

---
+++ src/Egulias/EmailValidator/Validation/SpoofCheckValidation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Egulias\EmailValidator\Validation;
+
+use Egulias\EmailValidator\EmailLexer;
+use Egulias\EmailValidator\Validation\Error\SpoofEmail;
+use \Spoofchecker;
+
+class SpoofCheckValidation implements EmailValidation
+{
+    /**
+     * @var InvalidEmail
+     */
+    private $error;
+
+    public function isValid($email, EmailLexer $emailLexer)
+    {
+        $checker = new Spoofchecker();
+        $checker->setChecks(Spoofchecker::SINGLE_SCRIPT);
+        
+        if ($checker->isSuspicious($email)) {
+            $this->error = new SpoofEmail();
+            return false;
+        }
+        
+        return true;
+    }
+
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    public function getWarnings()
+    {
+        return [];
+    }
+}
