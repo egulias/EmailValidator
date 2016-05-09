e4f04d5d8a26f54ae0102b3f326adac920020ff3
Eduardo Gulias Davis <eduardomgulias@gmail.com>
2016-05-09 23:55:12 +0200

    Spoof validation (#100)
    * Spoof validation

---
+++ Tests/EmailValidator/Validation/SpoofCheckValidationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Egulias\Tests\EmailValidator\Validation;
+
+use Egulias\EmailValidator\EmailLexer;
+use Egulias\EmailValidator\Validation\SpoofCheckValidation;
+
+class SpoofCheckValidationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider validUTF8EmailsProvider
+     */
+    public function testUTF8EmailAreValid($email)
+    {
+        $validation = new SpoofCheckValidation();
+        
+        $this->assertTrue($validation->isValid($email, new EmailLexer()));
+    }
+    
+    public function testEmailWithSpoofsIsInvalid()
+    {
+        $validation = new SpoofCheckValidation();
+
+        $this->assertFalse($validation->isValid("Кириллица"."latin漢字"."ひらがな"."カタカナ", new EmailLexer()));
+    }
+    
+    public function validUTF8EmailsProvider()
+    {
+        return [
+            // Cyrillic
+            ['Кириллица@Кириллица'],
+            // Latin + Han + Hiragana + Katakana
+            ["latin漢字"."ひらがな"."カタカナ"."@example.com"],
+            // Latin + Han + Hangul
+            ["latin"."漢字"."조선말"."@example.com"],
+            // Latin + Han + Bopomofo
+            ["latin"."漢字"."ㄅㄆㄇㄈ"."@example.com"]
+        ];
+    }
+}
