c1010f40300034409dfd00e0f37dd3263ecd5384
Issei.M <issei.m7@gmail.com>
2016-06-22 22:25:56 +0900

    fixed some problems in DNSCheckValidation

--- EmailValidator/Validation/DNSCheckValidation.php
+++ EmailValidator/Validation/DNSCheckValidation.php
@@ -27,10 +27,10 @@ class DNSCheckValidation implements EmailValidation
     {
         // use the input to check DNS if we cannot extract something similar to a domain
         $host = $email;
+
         // Arguable pattern to extract the domain. Not aiming to validate the domain nor the email
-        $pattern = "/^[a-z'0-9]+([+._-][a-z'0-9]+)*@([a-z0-9]+([._-][a-z0-9]+)+)+$/";
-        if (preg_match($pattern, $email, $result)) {
-            $host = $this->extractHost($result);
+        if (false !== $lastAtPos = strrpos($email, '@')) {
+            $host = substr($email, $lastAtPos + 1);
         }
 
         return $this->checkDNS($host);
@@ -46,16 +46,6 @@ class DNSCheckValidation implements EmailValidation
         return $this->warnings;
     }
 
-    private function extractHost(array $result)
-    {
-        foreach ($result as $match) {
-            $onlyDomainPattern = "/^([a-z0-9]+([._-][a-z0-9]+))+$/";
-            if (preg_match($onlyDomainPattern, $match, $domainResult)) {
-                return $domainResult[0];
-            }
-        }
-    }
-
     protected function checkDNS($host)
     {
         $Aresult = true;
@@ -63,7 +53,7 @@ class DNSCheckValidation implements EmailValidation
         
         if (!$MXresult) {
             $this->warnings[NoDNSMXRecord::CODE] = new NoDNSMXRecord();
-            $Aresult = checkdnsrr($host, 'A');
+            $Aresult = checkdnsrr($host, 'A') || checkdnsrr($host, 'AAAA');
             if (!$Aresult) {
                 $this->error = new NoDNSRecord();
             }
