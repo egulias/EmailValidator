19811e00829958f06d9bfbfeb0d0e6b7aaff3767
Issei.M <issei.m7@gmail.com>
2016-07-12 22:22:10 +0900

    MultipleValidatorWithAnd now can break out of loop when error occurs (#122)
    * MultipleValidatorWithAnd now can break out of loop when error occurs
    
    * improved logic
    
    * updated doc
    
    * bugfix

--- EmailValidator/Validation/MultipleValidationWithAnd.php
+++ EmailValidator/Validation/MultipleValidationWithAnd.php
@@ -3,23 +3,60 @@
 namespace Egulias\EmailValidator\Validation;
 
 use Egulias\EmailValidator\EmailLexer;
+use Egulias\EmailValidator\Exception\InvalidEmail;
 use Egulias\EmailValidator\Validation\Exception\EmptyValidationList;
 
 class MultipleValidationWithAnd implements EmailValidation
 {
+    /**
+     * If one of validations gets failure skips all succeeding validation.
+     * This means MultipleErrors will only contain a single error which first found.
+     */
+    const STOP_ON_ERROR = 0;
+
+    /**
+     * All of validations will be invoked even if one of them got failure.
+     * So MultipleErrors will contain all causes.
+     */
+    const ALLOW_ALL_ERRORS = 1;
+
+    /**
+     * @var EmailValidation[]
+     */
     private $validations = [];
+
+    /**
+     * @var array
+     */
     private $warnings = [];
+
+    /**
+     * @var MultipleErrors
+     */
     private $error;
-    
-    public function __construct(array $validations)
+
+    /**
+     * @var bool
+     */
+    private $mode;
+
+    /**
+     * @param EmailValidation[] $validations The validations.
+     * @param int               $mode        The validation mode (one of the constants).
+     */
+    public function __construct(array $validations, $mode = self::ALLOW_ALL_ERRORS)
     {
         if (count($validations) == 0) {
             throw new EmptyValidationList();
         }
         
         $this->validations = $validations;
+        $this->mode = $mode;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function isValid($email, EmailLexer $emailLexer)
     {
         $result = true;
@@ -29,17 +66,32 @@ class MultipleValidationWithAnd implements EmailValidation
             $result = $result && $validation->isValid($email, $emailLexer);
             $this->warnings = array_merge($this->warnings, $validation->getWarnings());
             $errors[] = $validation->getError();
+
+            if ($this->shouldStop($result)) {
+                break;
+            }
         }
         $this->error = new MultipleErrors($errors);
         
         return $result;
     }
 
+    private function shouldStop($result)
+    {
+        return !$result && $this->mode === self::STOP_ON_ERROR;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getError()
     {
         return $this->error;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getWarnings()
     {
         return $this->warnings;
