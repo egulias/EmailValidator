4d446bf971d0e25f44c52320b33e60acefb4657d
Issei.M <issei.m7@gmail.com>
2016-08-04 05:42:12 +0900

    NoRFCWarningsValidation now has an InvalidEmail instance if invalid email is passed (#126)
    * NoRFCWarningsValidation now has an InvalidEmail instance if invalid email is passed
    
    * fix mentioned point

--- EmailValidator/Validation/NoRFCWarningsValidation.php
+++ EmailValidator/Validation/NoRFCWarningsValidation.php
@@ -3,11 +3,39 @@
 namespace Egulias\EmailValidator\Validation;
 
 use Egulias\EmailValidator\EmailLexer;
+use Egulias\EmailValidator\Exception\InvalidEmail;
+use Egulias\EmailValidator\Validation\Error\RFCWarnings;
 
 class NoRFCWarningsValidation extends RFCValidation
 {
+    /**
+     * @var InvalidEmail
+     */
+    private $error;
+
+    /**
+     * {@inheritdoc}
+     */
     public function isValid($email, EmailLexer $emailLexer)
     {
-        return parent::isValid($email, $emailLexer) && empty($this->getWarnings());
+        if (!parent::isValid($email, $emailLexer)) {
+            return false;
+        }
+
+        if (empty($this->getWarnings())) {
+            return true;
+        }
+
+        $this->error = new RFCWarnings();
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getError()
+    {
+        return $this->error ?: parent::getError();
     }
 }
