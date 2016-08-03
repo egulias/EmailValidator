6a18198510ef875cbf88f82f3e706d6a4450730a
Issei.M <issei.m7@gmail.com>
2016-08-03 22:22:29 +0900

    fix mentioned point

--- EmailValidator/Validation/Error/RFCWarnings.php
+++ EmailValidator/Validation/Error/RFCWarnings.php
@@ -4,11 +4,8 @@ namespace Egulias\EmailValidator\Validation\Error;
 
 use Egulias\EmailValidator\Exception\InvalidEmail;
 
-/**
- * @author Issei Murasawa <issei.m7@gmail.com>
- */
 class RFCWarnings extends InvalidEmail
 {
-    const code = 997;
-    const REASON = 'The email has no error with RFC, but some warnings.';
+    const CODE = 997;
+    const REASON = 'Warnings were found.';
 }
