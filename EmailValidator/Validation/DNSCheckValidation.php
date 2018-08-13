9dde0df1e358860343b72b9d8d16b82701cb2a0c
Tom Sommer <tomsommer@users.noreply.github.com>
2018-08-13 14:43:24 +0200

    DNSCheckValidation requires the Intl extension (#175)
    * DNSCheckValidation requires the Intl extension as well
    
    * Check for Intl extension in DNSCheckValidation

--- EmailValidator/Validation/DNSCheckValidation.php
+++ EmailValidator/Validation/DNSCheckValidation.php
@@ -18,6 +18,13 @@ class DNSCheckValidation implements EmailValidation
      * @var InvalidEmail
      */
     private $error;
+    
+    public function __construct()
+    {
+        if (!extension_loaded('intl')) {
+            throw new \LogicException(sprintf('The %s class requires the Intl extension.', __CLASS__));
+        }
+    }
 
     public function isValid($email, EmailLexer $emailLexer)
     {
