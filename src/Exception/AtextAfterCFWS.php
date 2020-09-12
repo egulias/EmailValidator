1f6c67451065750567f7d6efc78ba0d251282a96
Eduardo Gulias Davis <eduardomgulias@gmail.com>
2020-09-12 15:45:30 +0200

    Removed obsolete exceptions

--- src/Exception/AtextAfterCFWS.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Egulias\EmailValidator\Exception;
-
-class AtextAfterCFWS extends InvalidEmail
-{
-    const CODE = 133;
-    const REASON = "ATEXT found after CFWS";
-}
--- src/Exception/CharNotAllowed.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Egulias\EmailValidator\Exception;
-
-class CharNotAllowed extends InvalidEmail
-{
-    const CODE = 201;
-    const REASON = "Non allowed character in domain";
-}
