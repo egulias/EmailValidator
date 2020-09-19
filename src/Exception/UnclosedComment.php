c7ab280976348a6035ea9f501f664bfb439e2a30
zloKOMAtic <zlokomatic@users.noreply.github.com>
2020-09-19 16:37:41 +0200

    Update UnclosedComment.php (#263)

--- src/Exception/UnclosedComment.php
+++ src/Exception/UnclosedComment.php
@@ -5,5 +5,5 @@ namespace Egulias\EmailValidator\Exception;
 class UnclosedComment extends InvalidEmail
 {
     const CODE = 146;
-    const REASON = "No colosing comment token found";
+    const REASON = "No closing comment token found";
 }
