5fa792ad1853ae2bc60528dd3e5cbf4542d3c1df
David <davpsh@users.noreply.github.com>
2020-11-01 03:37:35 +0700

    Email addresses with single quote should not be valid (#268)
    * Fixes issue for email addresses with single quote in domain part
    * specify version in cli for older php versions
    
    Co-authored-by: Eduardo Gulias Davis <eduardomgulias@gmail.com>

--- src/Parser/DomainPart.php
+++ src/Parser/DomainPart.php
@@ -335,6 +335,7 @@ class DomainPart extends Parser
     {
         $invalidDomainTokens = array(
             EmailLexer::S_DQUOTE => true,
+            EmailLexer::S_SQUOTE => true,
             EmailLexer::S_SEMICOLON => true,
             EmailLexer::S_GREATERTHAN => true,
             EmailLexer::S_LOWERTHAN => true,
