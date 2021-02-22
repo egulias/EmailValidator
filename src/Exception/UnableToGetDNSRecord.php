1736cbc6f761ce38c9f8673403459ae2c727221c
Alies Lapatsin <web.lapateen@gmail.com>
2021-02-22 11:42:38 +0300

    #256 Make UnableToGetDNSRecord backward compatible

--- src/Exception/UnableToGetDNSRecord.php
+++ src/Exception/UnableToGetDNSRecord.php
@@ -2,7 +2,7 @@
 
 namespace Egulias\EmailValidator\Exception;
 
-final class UnableToGetDNSRecord extends InvalidEmail
+final class UnableToGetDNSRecord extends NoDNSRecord
 {
     const CODE = 3;
     const REASON = 'Unable to get DNS records for the host';
