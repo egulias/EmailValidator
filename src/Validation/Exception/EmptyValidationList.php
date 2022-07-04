fc2096416017152295b2c4f67d82a7705a607fcb
MathiasReker <mathias@reker.dk>
2022-07-04 18:20:19 +0200

    Nullable type declaration for default null value
    Adds ? before type declarations for parameters with a default null value

--- src/Validation/Exception/EmptyValidationList.php
+++ src/Validation/Exception/EmptyValidationList.php
@@ -9,7 +9,7 @@ class EmptyValidationList extends \InvalidArgumentException
     /**
     * @param int $code
     */
-    public function __construct($code = 0, Exception $previous = null)
+    public function __construct($code = 0, ?Exception $previous = null)
     {
         parent::__construct("Empty validation list is not allowed", $code, $previous);
     }
