877577656387a3ffabae60dd4c425059179834cb
Daniel <mail@danielkesselberg.de>
2023-10-22 23:56:40 +0200

    chore: replace scalar with more precise type specification (#369)
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- src/Warning/QuotedPart.php
+++ src/Warning/QuotedPart.php
@@ -2,16 +2,26 @@
 
 namespace Egulias\EmailValidator\Warning;
 
+use UnitEnum;
+
 class QuotedPart extends Warning
 {
     public const CODE = 36;
 
     /**
-     * @param scalar|null $prevToken
-     * @param scalar|null $postToken
+     * @param UnitEnum|string|int|null $prevToken
+     * @param UnitEnum|string|int|null $postToken
      */
     public function __construct($prevToken, $postToken)
     {
+        if ($prevToken instanceof UnitEnum) {
+            $prevToken = $prevToken->name;
+        }
+
+        if ($postToken instanceof UnitEnum) {
+            $postToken = $postToken->name;
+        }
+
         $this->message = "Deprecated Quoted String found between $prevToken and $postToken";
     }
 }
