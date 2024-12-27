f31e565efdc93f3039ae0ae0f4e55a68aeebd628
Eduardo Gulias Davis <eduardomgulias@gmail.com>
2024-12-27 22:55:04 +0100

    Polish

--- src/Result/Result.php
+++ src/Result/Result.php
@@ -6,22 +6,26 @@ interface Result
 {
     /**
      * Is validation result valid?
+     * 
      */
-    public function isValid() : bool;
+    public function isValid(): bool;
 
     /**
      * Is validation result invalid?
      * Usually the inverse of isValid()
+     * 
      */
-    public function isInvalid() : bool;
+    public function isInvalid(): bool;
 
     /**
      * Short description of the result, human readable.
+     * 
      */
-    public function description() : string;
+    public function description(): string;
 
     /**
      * Code for user land to act upon.
+     * 
      */
-    public function code() : int;
+    public function code(): int;
 }
