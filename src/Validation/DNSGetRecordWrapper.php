d164dab97f98b9b2de915d9d77922e6429318e7b
Eduardo Gulias Davis <eduardomgulias@gmail.com>
2024-12-31 16:18:02 +0100

    Polish

--- src/Validation/DNSGetRecordWrapper.php
+++ src/Validation/DNSGetRecordWrapper.php
@@ -15,7 +15,7 @@ class DNSGetRecordWrapper
         // A workaround to fix https://bugs.php.net/bug.php?id=73149
         set_error_handler(
             static function (int $errorLevel, string $errorMessage): never {
-                throw new \RuntimeException("Unable to get DNS record for the host: $errorMessage");
+                throw new \RuntimeException(sprintf('Unable to get DNS record for the host: %s', $errorMessage));
             }
         );
         try {
